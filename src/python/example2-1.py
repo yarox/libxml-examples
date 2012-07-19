5b0c11ccd291594c1f73515b30bb493b4defdeeb
Adrian Martin <adrianmartinparra@gmail.com>
2012-07-19 11:28:35 +0200

    Changed how a node is printed to stdout

--- src/python/example2-1.py
+++ src/python/example2-1.py
@@ -4,6 +4,7 @@ Create an XML document including different nodes.
 '''
 
 from lxml import etree
+from sys import stdout
 
 
 docname = '../../data/out2-1py.xml'
@@ -14,8 +15,6 @@ child2 = etree.SubElement(root, 'child2', foo='foo', bar='bar')
 child3 = etree.SubElement(child2, 'child3')
 child3.text = 'some text'
 
-print(etree.tostring(root, pretty_print=True, xml_declaration=True, 
-                      encoding='utf-8'))
-
 tree = etree.ElementTree(root)
+tree.write(stdout, pretty_print=True, xml_declaration=True, encoding='utf-8')
 tree.write(docname, pretty_print=True, xml_declaration=True, encoding='utf-8')
