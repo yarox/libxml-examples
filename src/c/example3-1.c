9aedf999d84344e5039ffc61442913b685ba408f
Adrian Martin <adrianmartinparra@gmail.com>
2012-07-23 10:26:26 +0300

    Cosmetic changes to example3-1.c

--- src/c/example3-1.c
+++ src/c/example3-1.c
@@ -20,44 +20,44 @@ char
 	char *end;
 
 
-  	// Trim leading space
+	// Trim leading space
   	while(isspace(*str)) str++;
 
 	// All spaces?
-  	if(*str == 0)
-    return NULL;
+	if(*str == 0)
+	return NULL;
 
-  	// Trim trailing space
-  	end = str + strlen(str) - 1;
-  	while(end > str && isspace(*end)) end--;
+	// Trim trailing space
+	end = str + strlen(str) - 1;
+	while(end > str && isspace(*end)) end--;
 
-  	// Write new null terminator
-  	*(end + 1) = 0;
+	// Write new null terminator
+	*(end + 1) = 0;
 
-  return str;
+	return str;
 }
 
 static void
 print_text_nodes(xmlNode *a_node)
 {
-    xmlNode *cur_node = NULL;
-    xmlChar *content;
+	xmlNode *cur_node = NULL;
+	xmlChar *content;
 
 
-    for (cur_node = a_node; cur_node; cur_node = cur_node->next) 
-    {
-        if (cur_node->type == XML_TEXT_NODE) 
-        {
-            content = strip(xmlNodeGetContent(cur_node));
+	for (cur_node = a_node; cur_node; cur_node = cur_node->next) 
+	{
+		if (cur_node->type == XML_TEXT_NODE) 
+		{
+			content = strip(xmlNodeGetContent(cur_node));
             
-            if (content != NULL)
-            {
-                printf("%s: %s\n", cur_node->parent->name, content);
-            }
-        }
+			if (content != NULL)
+			{
+				printf("%s: %s\n", cur_node->parent->name, content);
+			}
+		}
 
-        print_text_nodes(cur_node->children);
-    }
+		print_text_nodes(cur_node->children);
+	}
 }
 
 xmlDocPtr
@@ -106,7 +106,7 @@ getnodeset (xmlDocPtr doc, xmlChar *xpath, xmlChar *ns_prefix, xmlChar *ns_uri)
 	if(xmlXPathNodeSetIsEmpty(result->nodesetval))
 	{
 		xmlXPathFreeObject(result);
-        printf("No result\n");
+		printf("No result\n");
 		return NULL;
 	}
 	
@@ -116,59 +116,59 @@ getnodeset (xmlDocPtr doc, xmlChar *xpath, xmlChar *ns_prefix, xmlChar *ns_uri)
 int
 main (int argc, char **argv)
 {
-    char *docname = "../../data/simple.xml", *id, *color;
-    int i, size, num_children;
-    xmlXPathObjectPtr result;
-    xmlNodeSetPtr nodeset;
-    xmlChar *keyword;
-    xmlNodePtr node;
-    xmlDocPtr doc;
+	char *docname = "../../data/simple.xml", *id, *color;
+	int i, size, num_children;
+	xmlXPathObjectPtr result;
+	xmlNodeSetPtr nodeset;
+	xmlChar *keyword;
+	xmlNodePtr node;
+	xmlDocPtr doc;
     
         
 	doc = getdoc(docname);
 	
 	// Retrieve all room elements and print tag, 'id' property, and number of 
-    // children
+	// children
 	result = getnodeset(doc, BAD_CAST "/house/room", NULL, NULL);
 
-    if (result)
-    {     
-        for(i = 0; i < result->nodesetval->nodeNr; i++)
-        {  
-            node = result->nodesetval->nodeTab[i];
-            num_children = (int) xmlChildElementCount(node);
-            id = xmlGetProp(node, BAD_CAST "id");
+	if (result)
+	{     
+		for(i = 0; i < result->nodesetval->nodeNr; i++)
+		{  
+			node = result->nodesetval->nodeTab[i];
+			num_children = (int) xmlChildElementCount(node);
+			id = xmlGetProp(node, BAD_CAST "id");
             
-            printf("%s: a %s with %i objects\n", node->name, id, num_children);
-        }
+			printf("%s: a %s with %i objects\n", node->name, id, num_children);
+		}
         
-        xmlXPathFreeObject(result);
+		xmlXPathFreeObject(result);
     }
     
-    // Retrieve all red chairs ('chair' nodes with property 'color' = 'red') and 
-    // print color, name, and room
+	// Retrieve all red chairs ('chair' nodes with property 'color' = 'red') and 
+	// print color, name, and room
 	result = getnodeset(doc, BAD_CAST "//chair[@color='red']", NULL, NULL);
 
-    if (result)
-    {
-        for(i = 0; i < result->nodesetval->nodeNr; i++)
-        {  
-            node = result->nodesetval->nodeTab[i];
-            num_children = (int) xmlChildElementCount(node);
-            color = xmlGetProp(node, BAD_CAST "color");
-            id = xmlGetProp(node->parent, BAD_CAST "id");
+	if (result)
+	{
+		for(i = 0; i < result->nodesetval->nodeNr; i++)
+		{  
+			node = result->nodesetval->nodeTab[i];
+			num_children = (int) xmlChildElementCount(node);
+			color = xmlGetProp(node, BAD_CAST "color");
+			id = xmlGetProp(node->parent, BAD_CAST "id");
 
-            printf("a %s %s from the %s\n", color, node->name, id);
-        }
+			printf("a %s %s from the %s\n", color, node->name, id);
+	}
         
-        xmlXPathFreeObject(result);
-    }
+		xmlXPathFreeObject(result);
+	}
     
-    // List nodes whith text, if any
-    print_text_nodes(xmlDocGetRootElement(doc));
+	// List nodes whith text, if any
+	print_text_nodes(xmlDocGetRootElement(doc));
 
-    xmlFreeDoc(doc);
-    xmlCleanupParser();
+	xmlFreeDoc(doc);
+	xmlCleanupParser();
     
-    return(0);
+	return(0);
 }
